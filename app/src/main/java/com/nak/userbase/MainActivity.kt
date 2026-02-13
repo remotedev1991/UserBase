c20d0a1d5d2d39869fdd1f148450186b203a6635
vpsingh <remotedev1991@gmail.com>
2026-02-14 01:51:16 +0530

    Added foreground service example

--- app/src/main/java/com/nak/userbase/MainActivity.kt
+++ app/src/main/java/com/nak/userbase/MainActivity.kt
@@ -1,15 +1,23 @@
 package com.nak.userbase
 
+import android.Manifest
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.ComponentName
 import android.content.Context
+import android.content.Context.NOTIFICATION_SERVICE
 import android.content.Intent
 import android.content.ServiceConnection
 import android.os.Bundle
 import android.os.IBinder
 import android.util.Log
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -17,10 +25,13 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.navigation.compose.rememberNavController
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.nak.userbase.presentation.LocalUser
@@ -45,34 +56,90 @@ class MainActivity : ComponentActivity() {
         setContent {
             UserBaseTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    CompositionLocalProvider(
-                        LocalUser provides User("Vik")
+                    Greeting(
+                        name = "Android", Modifier.padding(innerPadding)
                     ) {
-                        LoginScreen(
-                            googleSignInClient = googleSignInClient,
-                            modifier = Modifier.padding(innerPadding),
-                            onLoginSuccess = {}, onLogoutSuccess = {},
-                            serviceConnectionHelper = ServiceConnectionHelper(this)
-                        )
+                        val notificaiton = createNotification()
+                        val manager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+                        manager.notify(10, notificaiton)
                     }
                 }
             }
         }
     }
+
+    fun createNotification(): Notification {
+
+        val channelId = "media_playback_test"
+
+        val channel = NotificationChannel(
+            channelId,
+            "media_test",
+            NotificationManager.IMPORTANCE_HIGH
+        )
+
+        val manager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        manager.createNotificationChannel(channel)
+
+        //handle the click on the notification
+        val pendingIntent = PendingIntent.getActivity(
+            this@MainActivity,
+            100,
+            Intent(this, MainActivity::class.java), //this is the target activity
+            PendingIntent.FLAG_IMMUTABLE
+        )
+
+        //Builder pattern
+        return Notification.Builder(this, channelId)
+            .setContentTitle("Testing")
+            .setContentText("Testing Notification body")
+            .setSmallIcon(R.drawable.ic_launcher_background)
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+            .build()
+    }
+
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun Greeting(name: String, modifier: Modifier = Modifier, onButtonClick: () -> Unit) {
+    val context = LocalContext.current
+
+    val launcher = rememberLauncherForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions()
+    ) { permissionsMap ->
+        val areGranted = permissionsMap.values.reduce { acc, next -> acc && next }
+        if (areGranted) {
+            context.startForegroundService(Intent(context, MediaPlayBackService::class.java))
+        }
+    }
+
+    LaunchedEffect(Unit) {
+        launcher.launch(
+            arrayOf(
+                Manifest.permission.FOREGROUND_SERVICE,
+                Manifest.permission.FOREGROUND_SERVICE_MEDIA_PLAYBACK,
+                Manifest.permission.POST_NOTIFICATIONS
+            )
+        )
+    }
+
     Text(
         text = "Hello $name!",
-        modifier = modifier
+        modifier = modifier.clickable {
+            onButtonClick()
+        }
     )
 }
+
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     UserBaseTheme {
-        Greeting("Android")
+        Greeting("Android") {
+
+        }
     }
 }
--- app/src/main/java/com/nak/userbase/presentation/components/MediaPlayBackService.kt
+++ app/src/main/java/com/nak/userbase/presentation/components/MediaPlayBackService.kt
@@ -1,12 +1,56 @@
 package com.nak.userbase.presentation.components
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.app.Service
 import android.content.Intent
 import android.os.Binder
 import android.os.IBinder
 import android.util.Log
+import com.nak.userbase.MainActivity
+import com.nak.userbase.R
 
 class MediaPlayBackService : Service() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        startForeground(1, createNotification())
+    }
+
+    fun createNotification(): Notification {
+
+        val channelId = "media_playback"
+
+        val channel = NotificationChannel(
+            channelId,
+            "media",
+            NotificationManager.IMPORTANCE_HIGH
+        )
+
+        val manager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+
+        manager.createNotificationChannel(channel)
+
+
+        //handle the click on the notification
+        val pendingIntent = PendingIntent.getActivity(
+            this,
+            100,
+            Intent(this, MainActivity::class.java), //this is the target activity
+            PendingIntent.FLAG_IMMUTABLE
+        )
+
+        //Builder pattern
+        return Notification.Builder(this, channelId)
+            .setContentTitle("Media")
+            .setContentText("Notification body")
+            .setSmallIcon(R.drawable.ic_launcher_background)
+            .setContentIntent(pendingIntent)
+            .build()
+    }
 
     private val binder = LocalBinder()
 
@@ -18,6 +62,13 @@ class MediaPlayBackService : Service() {
 
     override fun onBind(intent: Intent): IBinder {
         return binder
+    }
+
+    private val TAG = "MediaPlayBackService"
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy: ")
     }
 
     fun testServiceFunction(): String {
